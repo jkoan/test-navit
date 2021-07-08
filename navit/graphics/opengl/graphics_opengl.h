44cb7a725250d275c0b8b2cf3963884632ddd80e
jkoan <jkoan@users.noreply.github.com>
2021-07-08 07:46:56 +0200

    Update graphics_opengl.h

--- navit/graphics/opengl/graphics_opengl.h
+++ navit/graphics/opengl/graphics_opengl.h
@@ -2,15 +2,19 @@ struct graphics_opengl_window_system;
 struct graphics_opengl_platform;
 
 
-struct graphics_opengl_window_system_methods {
+struct graphics_opengl_window_system_methods 
+{
 	void (*destroy)(struct graphics_opengl_window_system *);
 	void *(*get_display)(struct graphics_opengl_window_system *);
 	void *(*get_window)(struct graphics_opengl_window_system *);
 	void (*set_callbacks)(struct graphics_opengl_window_system *, void *data, void *resize, void *button, void *motion, void *keypress);
 };
 
-struct graphics_opengl_platform_methods {
-	void (*destroy)(struct graphics_opengl_platform *);
+struct graphics_opengl_platform_methods 
+ {
+	void 
+		(*destroy)
+		(struct graphics_opengl_platform *);
 	void (*swap_buffers)(struct graphics_opengl_platform *);
 };
 
